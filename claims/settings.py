3e8d62d8a070536e3f67140d32267fc0fbb1b239
Elijah <mukisaelijah293@gmail.com>
2025-01-04 02:52:22 +0300

    Updates

--- claims/settings.py
+++ claims/settings.py
@@ -102,6 +102,12 @@ WSGI_APPLICATION = 'claims.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 DATABASES = {
     'default': {
@@ -114,12 +120,6 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 
